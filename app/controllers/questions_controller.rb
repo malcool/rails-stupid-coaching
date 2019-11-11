cda3fdc28a9a9980ffdb2aa1814887b640965cc0
Malcolm Remple <hello@malcolmremple.com>
2019-11-11 16:23:11 -0500

    done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,11 +3,12 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    # raise
     @question = params[:question]
     if @question == "i am going to work right now!"
-      @answer = "Cool"
-      # elsif params[:question].end_with?("?")
-      # "Silly question, get dressed and go to work!"
+      @answer = "Great!"
+    elsif params[:question].end_with?("?")
+      "Silly question, get dressed and go to work!"
     else
       @answer = "I don't care, get dressed and go to work!"
     end
